d71aa525bb02a676660811a888f4a80f6063a4a6
Leandro Motta Barros <lmb@stackedboxes.org>
2018-04-21 12:16:12 -0300

    Add a character that can even move! Wow!

---
+++ src/scenes/character/player.gd
@@ -0,0 +1,25 @@
+extends "res://scenes/character/base_character.gd"
+
+
+func _process(delta):
+	var velocity = Vector2()
+
+	# Walk
+	if Input.is_action_pressed("right"):
+		velocity.x += 1
+	if Input.is_action_pressed("left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("down"):
+		velocity.y += 1
+	if Input.is_action_pressed("up"):
+		velocity.y -= 1
+
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		
+	# Rotate before magnet influence
+	if velocity.length_squared() > 0:
+		rotation = velocity.angle() + PI/2
+
+	# Move!
+	move_and_slide(velocity)
